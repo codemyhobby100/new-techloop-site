aaf5d2c07051bc4ee4117c89b5753fa99b89c165
Codemyhobby <alhassandamilola16@gmail.com>
2024-11-18 16:58:22 +0000

    update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <section>
         <div className="max-w-7xl mx-auto px-4 mt-20">
-            <LampContainer className='mt-[-100px]'>
+            <LampContainer className='mt-[-200px]'>
             <motion.h2
                 initial={{ opacity: 0.5, y: 100 }}
                 whileInView={{ opacity: 1, y: 0 }}
@@ -15,7 +15,8 @@ const Contact = () => {
                 duration: 0.8,
                 ease: "easeInOut",
                 }}
-                className="mt-8 bg-gradient-to-br    from-neutral-50 via-neutral-300 to-neutral-600  py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+                className="mt-3 bg-gradient-to-br from-neutral-50 via-neutral-300 to-neutral-600
+                py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
             >
                 Get in Touch <br /> Bring Your Vision to Life
             </motion.h2>
